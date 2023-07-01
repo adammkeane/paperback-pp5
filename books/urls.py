73afcb328af64f47fa99dc8e614898b7b2da5e66
adammkeane <>
2023-07-01 11:58:43 +0000

    Add basic books view and template

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home'),
+    path('', views.AllBooks.as_view(), name='books'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Book
 
-# Create your views here.
+
+# class AllBooks(generic.TemplateView):
+#     """View to show all books, including sorting and search queries"""
+#     template_name = 'books/books.html'
+
+
+class AllBooks(generic.ListView):
+    """View to show all books, including sorting and search queries"""
+    model = Book
+    queryset = Book.objects.order_by('name')
+    template_name = 'books/books.html'
+    paginate_by = 10
