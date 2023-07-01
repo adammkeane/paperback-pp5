a98b696c4149a5760e16fb733528e35cc3ffcd95
adammkeane <>
2023-07-01 13:48:28 +0000

    Change books view to function, instead of class

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.AllBooks.as_view(), name='books'),
+    path('', views.all_books, name='books'),
 ]
--- books/views.py
+++ books/views.py
@@ -8,9 +8,20 @@ from .models import Book
 #     template_name = 'books/books.html'
 
 
-class AllBooks(generic.ListView):
-    """View to show all books, including sorting and search queries"""
-    model = Book
-    queryset = Book.objects.order_by('name')
-    template_name = 'books/books.html'
-    paginate_by = 10
+# class AllBooks(generic.ListView):
+#     """View to show all books, including sorting and search queries"""
+#     model = Book
+#     queryset = Book.objects.order_by('name')
+#     template_name = 'books/books.html'
+#     paginate_by = 10
+
+
+def all_books(request):
+    """ View to show all books, including sorting and search queries """
+    books = Book.objects.all()
+
+    context = {
+        'books': books,
+    }
+
+    return render(request, 'books/books.html', context)
