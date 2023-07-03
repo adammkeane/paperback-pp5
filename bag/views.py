295dd4bd61bc046097d090a227c5076b1d062d57
adammkeane <>
2023-07-03 18:04:39 +0000

    Add basic update and remove button functionality on bag template

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
 
 
 def view_bag(request):
@@ -45,3 +46,65 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified bookto the specified amount"""
+
+    # book = get_object_or_404(Book, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    option = None
+    if 'book_option' in request.POST:
+        option = request.POST['book_option']
+    bag = request.session.get('bag', {})
+
+    if option:
+        if quantity > 0:
+            bag[item_id]['items_by_option'][option] = quantity
+            # messages.success(
+            #     request, f'Updated option {option.upper()} {book.name} quantity to {bag[item_id]["items_by_option"][option]}')
+        else:
+            del bag[item_id]['items_by_option'][option]
+            if not bag[item_id]['items_by_option']:
+                bag.pop(item_id)
+            # messages.success(
+            #     request, f'Removed option {option.upper()} {book.name} from your bag')
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+            # messages.success(
+            #     request, f'Updated {book.name} quantity to {bag[item_id]}')
+        else:
+            bag.pop(item_id)
+            # messages.success(request, f'Removed {book.name} from your bag')
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        # book = get_object_or_404(Book, pk=item_id)
+        option = None
+        if 'book_option' in request.POST:
+            option = request.POST['book_option']
+        bag = request.session.get('bag', {})
+
+        if option:
+            del bag[item_id]['items_by_option'][option]
+            if not bag[item_id]['items_by_option']:
+                bag.pop(item_id)
+            # messages.success(
+            #     request, f'Removed size {size.upper()} {product.name} from your bag')
+        else:
+            bag.pop(item_id)
+            # messages.success(request, f'Removed {product.name} from your bag')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
