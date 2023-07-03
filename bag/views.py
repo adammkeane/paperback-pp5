cb5355747c824e18a186c3d80f57ccc0e4b5e4e5
adammkeane <>
2023-07-03 10:42:13 +0000

    Add add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 def view_bag(request):
     """ View that renders page with shopping bag contents """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add quantity of selected item to shopping bag """
+
+    # book = get_object_or_404(Book, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    # option = None
+    # wh_secret = settings.STRIPE_WH_SECRET
+    # print(wh_secret)
+    # if 'book_option' in request.POST:
+    #     option = request.POST['book_option']
+
+    bag = request.session.get('bag', {})
+
+    # temp code
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    # if option:
+    #     if item_id in list(bag.keys()):
+    #         if option in bag[item_id]['items_by_option'].keys():
+    #             bag[item_id]['items_by_option'][option] += quantity
+    #             messages.success(
+    #                 request, f'Updated option {option.upper()} {book.name} quantity to {bag[item_id]["items_by_option"][option]}')
+    #         else:
+    #             bag[item_id]['items_by_option'][option] = quantity
+    #             messages.success(
+    #                 request, f'Added option {option.upper()} {book.name} to your bag')
+    #     else:
+    #         bag[item_id] = {'items_by_option': {option: quantity}}
+    #         messages.success(
+    #             request, f'Added option {option.upper()} {book.name} to your bag')
+    # else:
+    #     if item_id in list(bag.keys()):
+    #         bag[item_id] += quantity
+    #         messages.success(
+    #             request, f'Updated {book.name} quantity to {bag[item_id]}')
+    #     else:
+    #         bag[item_id] = quantity
+    #         messages.success(request, f'Added {book.name} to your bag')
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
