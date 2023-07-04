bb745eb98ed461df8de3c01baef2277190cacd93
adammkeane <>
2023-07-04 11:44:27 +0000

    Add shopping bag preview and extra messages to the bag view

--- bag/views.py
+++ bag/views.py
@@ -27,21 +27,21 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if option in bag[item_id]['items_by_option'].keys():
                 bag[item_id]['items_by_option'][option] += quantity
-    #             messages.success(
-    #                 request, f'Updated option {option.upper()} {book.name} quantity to {bag[item_id]["items_by_option"][option]}')
+                messages.success(
+                    request, f'Updated option {option.upper()} {book.name} quantity to {bag[item_id]["items_by_option"][option]}')
             else:
                 bag[item_id]['items_by_option'][option] = quantity
-    #             messages.success(
-    #                 request, f'Added option {option.upper()} {book.name} to your bag')
+                messages.success(
+                    request, f'Added {option.upper()} {book.name} to your bag')
         else:
             bag[item_id] = {'items_by_option': {option: quantity}}
-    #         messages.success(
-    #             request, f'Added option {option.upper()} {book.name} to your bag')
+            messages.success(
+                request, f'Added {option.upper()} {book.name} to your bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
-    #         messages.success(
-    #             request, f'Updated {book.name} quantity to {bag[item_id]}')
+            messages.success(
+                request, f'Updated {book.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {book.name} to your bag')
@@ -53,7 +53,7 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified bookto the specified amount"""
 
-    # book = get_object_or_404(Book, pk=item_id)
+    book = get_object_or_404(Book, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     option = None
     if 'book_option' in request.POST:
@@ -63,22 +63,22 @@ def adjust_bag(request, item_id):
     if option:
         if quantity > 0:
             bag[item_id]['items_by_option'][option] = quantity
-            # messages.success(
-            #     request, f'Updated option {option.upper()} {book.name} quantity to {bag[item_id]["items_by_option"][option]}')
+            messages.success(
+                request, f'Updated {option.upper()} {book.name} quantity to {bag[item_id]["items_by_option"][option]}')
         else:
             del bag[item_id]['items_by_option'][option]
             if not bag[item_id]['items_by_option']:
                 bag.pop(item_id)
-            # messages.success(
-            #     request, f'Removed option {option.upper()} {book.name} from your bag')
+            messages.success(
+                request, f'Removed {option.upper()} {book.name} from your bag')
     else:
         if quantity > 0:
             bag[item_id] = quantity
-            # messages.success(
-            #     request, f'Updated {book.name} quantity to {bag[item_id]}')
+            messages.success(
+                request, f'Updated {book.name} quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
-            # messages.success(request, f'Removed {book.name} from your bag')
+            messages.success(request, f'Removed {book.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -88,7 +88,7 @@ def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
     try:
-        # book = get_object_or_404(Book, pk=item_id)
+        book = get_object_or_404(Book, pk=item_id)
         option = None
         if 'book_option' in request.POST:
             option = request.POST['book_option']
@@ -98,11 +98,11 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_option'][option]
             if not bag[item_id]['items_by_option']:
                 bag.pop(item_id)
-            # messages.success(
-            #     request, f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(
+                request, f'Removed {option.upper()} {book.name} from your bag')
         else:
             bag.pop(item_id)
-            # messages.success(request, f'Removed {product.name} from your bag')
+            messages.success(request, f'Removed {book.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
