52a78e6222a7fb3eee1c1f8404711ef2ee58a91a
adammkeane <>
2023-07-07 13:42:42 +0000

    Add next order model fields

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -79,8 +79,8 @@ class StripeWH_Handler:
                     street_address2__iexact=shipping_details.address.line2,
                     county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
-                    # original_bag=bag,
-                    # stripe_pid=pid,
+                    original_bag=bag,
+                    stripe_pid=pid,
                 )
                 order_exists = True
                 break
@@ -106,8 +106,8 @@ class StripeWH_Handler:
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
                     county=shipping_details.address.state,
-                    # original_bag=bag,
-                    # stripe_pid=pid,
+                    original_bag=bag,
+                    stripe_pid=pid,
                 )
                 for item_id, item_data in json.loads(bag).items():
                     book = Book.objects.get(id=item_id)
