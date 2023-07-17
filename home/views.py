d78f0d781d861ab49a5513bd5720cdd6b8fb0659
adammkeane <>
2023-07-17 15:59:12 +0000

    Add latest 3 books to homepage

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import generic, View
+from django.views.generic import ListView
+from books.models import Book
 
 
-class HomePage(generic.TemplateView):
-    """View to show the home page"""
+class HomePage(ListView):
+    """View to show the home page, including the latest 3 books"""
     template_name = 'home/index.html'
+    queryset = Book.objects.all()[:3]
+    context_object_name = "books"
