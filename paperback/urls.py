8679c7c66f640b9ff9b569804f8cc96f600a528f
adammkeane <>
2023-07-17 16:38:49 +0000

    Add favicon 2

--- paperback/urls.py
+++ paperback/urls.py
@@ -1,3 +1,4 @@
+
 """paperback URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -17,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from .views import handler404
 
@@ -29,6 +32,8 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
+    path('favicon.ico', RedirectView.as_view(
+        url=staticfiles_storage.url('favicon/favicon.ico')))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'paperback.views.handler404'
