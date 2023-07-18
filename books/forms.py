dafabac50e9738d42e993cc1a82dd75c513c0f0a
adammkeane <>
2023-07-18 11:49:01 +0000

    Update css for book detail and author page

--- books/forms.py
+++ books/forms.py
@@ -12,15 +12,6 @@ class BookForm(forms.ModelForm):
     image = forms.ImageField(
         label='Image', required=False, widget=CustomClearableFileInput)
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     categories = Category.objects.all()
-    #     friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
-    #     self.fields['category'].choices = friendly_names
-    #     for field_name, field in self.fields.items():
-    #         field.widget.attrs['class'] = 'border-black rounded-0'
-
 
 class BookReviewForm(forms.ModelForm):
     class Meta:
